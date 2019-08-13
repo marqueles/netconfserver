1650a7df0e3d1c9b35369e142b28f275ecd61cd9
marcos <marcos.jimenezgutierrez@telefonica.com>
2019-08-13 12:06:11 +0200

    setup_binding.sh calls setup_dbs.py to create de pertinent collections

--- setup_files/setup_binding.sh
+++ setup_files/setup_binding.sh
@@ -2,14 +2,18 @@ if [ "$#" -ne "2" ]; then
   	echo "Sorry, you need to include a file with the datastores and another with the augments, aditionally you should ad a folder with the associated yang files(datastores, imports and augments) , called setup_files "
 	exit 1
 fi
-
-#sed -i -e 's/^/setup_files\//' $1
-#sed -i -e 's/^/setup_files\//' $2
 dependencies="$(cat $1)"
 dependencies="$dependencies $(cat $2)"
-echo $dependencies
-PYBINDPLUGIN=`/usr/bin/env python -c 'import pyangbind; import os; print ("{}/plugin".format(os.path.dirname(pyangbind.__file__)))'`
-echo $PYBINDPLUGIN
+#echo $dependencies
+
+PYBINDPLUGIN=$(/usr/bin/env python -c 'import pyangbind; import os; print ("{}/plugin".format(os.path.dirname(pyangbind.__file__)))')
+#echo $PYBINDPLUGIN
 
 pyang --plugindir $PYBINDPLUGIN -f pybind -o binding.py $dependencies
 cp binding.py ../
+
+
+cat $1 | while read -r line
+do
+  python setup_db.py "$line"
+done
