1f30a156d3164bf4e31d5274c2701fc09752b82a
marcos <marcos.jimenezgutierrez@telefonica.com>
2019-08-22 10:44:37 +0200

    Solved bug on setup_binding

--- tests/ClientGetConfigFilter.py
+++ tests/ClientGetConfigFilter.py
@@ -3,7 +3,7 @@ from netconf.client import NetconfSSHSession
 from lxml import etree
 
 session = NetconfSSHSession("localhost", "8300", "admin", "admin")
-config = session.get_config(select='<components xmlns="http://openconfig.net/yang/platform"><component><name>LINECARD-1-21</name><config/></component></components>')
+config = session.get_config(select='<components xmlns="http://openconfig.net/yang/platform"><component><name>shelf-sh41</name><config/></component></components>')
 
 #dom = xml.dom.minidom.parseString(config)
 
