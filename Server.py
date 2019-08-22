af537dfafdc557a84ca4c68683e20c794b799f79
marqueles <marcosjggarcia@gmail.com>
2019-08-22 10:58:53 +0200

    Get get config mongo update (#5)
    Added back mongodb functionality and implemented in rpc get and get-config.
    Edit config functionality with mongodb as TODO

--- Server.py
+++ Server.py
@@ -27,10 +27,16 @@ import sys
 import time
 from netconf import error, server, util
 from netconf import nsmap_add, NSMAP
-
+from pymongo import MongoClient
 from lxml import etree
+import xml.etree.ElementTree as ET
 from lxml import objectify
 import Validation
+import pyangbind.lib.serialise as serialise
+import pyangbind.lib.pybindJSON as pybindJSON
+from pyangbind.lib.serialise import pybindJSONDecoder
+import binding
+import json
 
 nsmap_add("sys", "urn:ietf:params:xml:ns:yang:ietf-system")
 
@@ -68,44 +74,156 @@ class SystemServer(object):
         util.subelm(capabilities, "capability").text = NSMAP["sys"]
 
     def rpc_get(self, session, rpc, filter_or_none):  # pylint: disable=W0613
-        logging.info(etree.tostring(rpc, pretty_print=True))
-        # Empty filter
-        if rpc[0].find('{urn:ietf:params:xml:ns:netconf:base:1.0}filter') is None:
+        if rpc[0].find('{*}filter') is None:
             # All configuration files should be appended
-            response_tree = etree.parse("configuration/platform.xml")
-            response = response_tree.getroot()
-        # Only supportedd datastore so far is platform
-        elif "platform" in rpc[0][1][0].tag:
-            response_tree = etree.parse("configuration/platform.xml")
-            # logging.info(etree.tostring(response_tree, pretty_print=True))
-            response = response_tree.getroot()
+            dbclient = MongoClient()
+            db = dbclient.netconfserver
+            data_elm = etree.Element('data', nsmap={None: 'urn:ietf:params:xml:ns:netconf:base:1.0'})
+            i = 1
+            logging.info(db.list_collections())
+            for collection_name in db.list_collection_names():
+                collection = getattr(db, collection_name)
+                collection_data = collection.find_one()
+                collection_data_1 = dict(collection_data)
+                for element in collection_data:
+                    if "id" in element:
+                        del collection_data_1[element]
+                collection_data = collection_data_1
 
+                collection_binding = pybindJSONDecoder.load_ietf_json(collection_data, binding, collection_name)
+                xml_data_string = serialise.pybindIETFXMLEncoder.serialise(collection_binding)
+                xml_data = etree.XML(xml_data_string)
+                data_elm.insert(i, xml_data)
+                i += 1
+            xml_response = data_elm
+
+        # Only supportedd datastore so far is platform
         else:
-            raise AttributeError("The requested datastore is not supported")
+            # Parsing the database name form the rpc tag namespace
+            db_base = rpc[0][1][0].tag.split('}')[0].split('/')[-1]
+            db_source = rpc[0][1][0].tag.split('/')[2].split('.')[0]
+            db_name = db_source + "-" + db_base
+            # logging.info(db_name)
+
+            # Finding the datastore requested
+            dbclient = MongoClient()
+            db = dbclient.netconfserver
+            names = db.list_collection_names()
+            # logging.info(names)
+
+            if db_name in names:
+
+                logging.info("Found the datastore requested")
+
+                # Retrieving the data from the datastore
+                datastore_name = db_name + ":" + rpc[0][1][0].tag.split('}')[1]
+                datastore_data = db[db_name].find_one()
+
+                datastore_data_1 = dict(datastore_data)
+                for element in datastore_data:
+                    if "id" in element:
+                        del datastore_data_1[element]
+                datastore_data = datastore_data_1
+
+                database_data_binding = pybindJSONDecoder.load_ietf_json(datastore_data, binding, db_name)
+                logging.info(database_data_binding)
+
+                # Parsing the data to xml
+                xml_data = serialise.pybindIETFXMLEncoder.serialise(database_data_binding)
+                xml_response = etree.XML(xml_data)
+
+            else:
+                raise AttributeError("The requested datastore is not supported")
 
         # Validation.validate_rpc(response, "get-config")
-        return util.filter_results(rpc, response, filter_or_none, self.server.debug)
+        toreturn = util.filter_results(rpc, xml_response, filter_or_none, self.server.debug)
+
+        if "data" not in toreturn.tag:
+            logging.info("data not header")
+            nsmap = {None: 'urn:ietf:params:xml:ns:netconf:base:1.0'}
+            data_elm = etree.Element('data', nsmap={None: 'urn:ietf:params:xml:ns:netconf:base:1.0'})
+            data_elm.insert(1, toreturn)
+            logging.info(etree.tostring(data_elm, pretty_print=True))
+            toreturn = data_elm
+
+        return toreturn
 
     def rpc_get_config(self, session, rpc, source_elm, filter_or_none):  # pylint: disable=W0613
 
-        logging.info(etree.tostring(rpc, pretty_print=True))
+        # logging.info(etree.tostring(rpc, pretty_print=True))
         # Empty filter
-        if rpc[0].find('{urn:ietf:params:xml:ns:netconf:base:1.0}filter') is None:
+        if rpc[0].find('{*}filter') is None:
             # All configuration files should be appended
-            response_tree = etree.parse("configuration/platform.xml")
-            response = response_tree.getroot()
-        # Only supportedd datastore so far is platform
-        elif "platform" in rpc[0][1][0].tag:
-            response_tree = etree.parse("configuration/platform.xml")
-            # logging.info(etree.tostring(response_tree, pretty_print=True))
-            response = response_tree.getroot()
+            dbclient = MongoClient()
+            db = dbclient.netconfserver
+            data_elm = etree.Element('data', nsmap={None: 'urn:ietf:params:xml:ns:netconf:base:1.0'})
+            i = 1
+            logging.info(db.list_collections())
+            for collection_name in db.list_collection_names():
+                collection = getattr(db,collection_name)
+                collection_data = collection.find_one()
+                collection_data_1 = dict(collection_data)
+                for element in collection_data:
+                    if "id" in element:
+                        del collection_data_1[element]
+                collection_data = collection_data_1
 
+                collection_binding = pybindJSONDecoder.load_ietf_json(collection_data, binding, collection_name)
+                xml_data_string = serialise.pybindIETFXMLEncoder.serialise(collection_binding)
+                xml_data = etree.XML(xml_data_string)
+                data_elm.insert(i,xml_data)
+                i += 1
+            xml_response = data_elm
+
+        # Only supportedd datastore so far is platform
         else:
-            raise AttributeError("The requested datastore is not supported")
+            # Parsing the database name form the rpc tag namespace
+            db_base = rpc[0][1][0].tag.split('}')[0].split('/')[-1]
+            db_source = rpc[0][1][0].tag.split('/')[2].split('.')[0]
+            db_name = db_source + "-" + db_base
+            #logging.info(db_name)
+
+            # Finding the datastore requested
+            dbclient = MongoClient()
+            db = dbclient.netconfserver
+            names = db.list_collection_names()
+            #logging.info(names)
+
+            if db_name in names:
+
+                logging.info("Found the datastore requested")
+
+                # Retrieving the data from the datastore
+                datastore_name = db_name + ":" + rpc[0][1][0].tag.split('}')[1]
+                datastore_data = db[db_name].find_one()
+
+                datastore_data_1 = dict(datastore_data)
+                for element in datastore_data:
+                    if "id" in element:
+                        del datastore_data_1[element]
+                datastore_data = datastore_data_1
+
+                database_data_binding = pybindJSONDecoder.load_ietf_json(datastore_data, binding, db_name)
+                logging.info(database_data_binding)
+
+                # Parsing the data to xml
+                xml_data = serialise.pybindIETFXMLEncoder.serialise(database_data_binding)
+                xml_response = etree.XML(xml_data)
+
+            else:
+                raise AttributeError("The requested datastore is not supported")
 
         # Validation.validate_rpc(response, "get-config")
-        toreturn = util.filter_results(rpc, response, filter_or_none, self.server.debug)
+        toreturn = util.filter_results(rpc, xml_response, filter_or_none, self.server.debug)
         util.trimstate(toreturn)
+
+        if "data" not in toreturn.tag:
+            logging.info("data not header")
+            nsmap = {None : 'urn:ietf:params:xml:ns:netconf:base:1.0'}
+            data_elm = etree.Element('data',nsmap={None : 'urn:ietf:params:xml:ns:netconf:base:1.0'})
+            data_elm.insert(1,toreturn)
+            logging.info(etree.tostring(data_elm,pretty_print=True))
+            toreturn = data_elm
 
         return toreturn
 
